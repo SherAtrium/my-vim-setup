b5e86387f03f353541fcb38d0cc4aa6c00b32db5
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2025-12-01 10:46:18 +0500

    feat: NeoVim setup with lazyvim, LSPs, and keymaps with integration of lazyvim

---
+++ lua/plugins/webdev-icons.lua
@@ -0,0 +1,8 @@
+-- ================================================================================================
+-- TITLE: nvim-web-devicons
+-- ABOUT: Provides Nerd Font icons (glyphs) for use by Neovim plugins
+-- LINKS: https://github.com/nvim-tree/nvim-web-devicons
+-- ================================================================================================
+
+
+return { "nvim-tree/nvim-web-devicons", opts = {} }
