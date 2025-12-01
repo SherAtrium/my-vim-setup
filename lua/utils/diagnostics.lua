b6f70a33dec65db84ab9e790fc70aecad065a02a
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2025-12-01 14:46:50 +0500

    feat: update diagnostics and lazy config

--- lua/utils/diagnostics.lua
+++ lua/utils/diagnostics.lua
@@ -8,14 +8,36 @@ local diagnostic_signs = {
 }
 
 M.setup = function()
+	-- Register signs
+	for type, icon in pairs(diagnostic_signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
+
 	vim.diagnostic.config({
-		signs = {
-			text = {
-				[vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
-				[vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
-				[vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
-				[vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
-			},
+		-- Show signs in the gutter
+		signs = true,
+
+		-- Show diagnostics as virtual text
+		virtual_text = {
+			severity = { min = vim.diagnostic.severity.HINT },
+			spacing = 4,
+			prefix = "●",
+		},
+		-- virtual_lines = true,
+
+		underline = true,
+
+		-- **IMPORTANT:** Update diagnostics WHILE TYPING
+		update_in_insert = true,
+
+		-- Priority for errors > warnings > hints
+		severity_sort = true,
+
+		float = {
+			border = "rounded",
+			source = "if_many",
+			focusable = false,
 		},
 	})
 end
