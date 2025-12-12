78b65a99ec02744614542d8ac0f37489c32e7c5a
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2025-12-12 11:12:13 +0500

    feat: removed unnecessary components from lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -100,13 +100,14 @@ function M.setup()
 				-- },
 			},
 			lualine_y = {
-				{ "progress", separator = " ", padding = { left = 1, right = 0 } },
-				{ "location", padding = { left = 0, right = 1 } },
+				-- { "progress", separator = " ", padding = { left = 1, right = 0 } },
+				-- { "location", padding = { left = 0, right = 1 } },
 			},
 			lualine_z = {
-				function()
-					return " " .. os.date("%R")
-				end,
+				{ "location", padding = { left = 1, right = 0 } },
+				-- function()
+				-- 	return " " .. os.date("%R")
+				-- end,
 			},
 		},
 	})
